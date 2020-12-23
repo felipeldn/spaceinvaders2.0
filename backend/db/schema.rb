d568a950bfa85109fe6556eeb0e61debc8898705
felipeldn91 <felipeldn91@gmail.com>
2020-12-23 20:10:56 +0000

    Password column removed

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_175956) do
+ActiveRecord::Schema.define(version: 2020_12_23_200831) do
 
   create_table "games", force: :cascade do |t|
     t.integer "score", default: 0
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2020_12_23_175956) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
